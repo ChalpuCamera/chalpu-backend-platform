525e97b1ffdae0757f26f1a8603f9e5950e1b031
GoToBILL <wnqudwnrlrl@naver.com>
2025-10-03 11:57:03 +0900

    feat: 모든 매장 조회 추가

--- src/main/java/com/example/chalpuplatform/store/repository/StoreRepository.java
+++ src/main/java/com/example/chalpuplatform/store/repository/StoreRepository.java
@@ -1,6 +1,8 @@
 package com.example.chalpuplatform.store.repository;
 
 import com.example.chalpuplatform.store.domain.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,6 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     Optional<Store> findByIdAndIsActiveTrue(Long id);
     
     List<Store> findByIsActiveTrue();
+
+    Page<Store> findByIsActiveTrue(Pageable pageable);
 } 
