63cc6aa2a100e18d4ab2b266e4b3e52c857c6e72
GoToBILL <wnqudwnrlrl@naver.com>
2025-10-16 01:25:31 +0900

    feat: add LOGIN

--- src/main/java/com/example/chalpuplatform/landing/domain/ButtonType.java
+++ src/main/java/com/example/chalpuplatform/landing/domain/ButtonType.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ButtonType {
     START_FREE("무료로 시작하기"),
-    KAKAO_LOGIN("카카오 로그인");
+    KAKAO_LOGIN("카카오 로그인"),
+    LOGIN("로그인");
 
     private final String description;
 
