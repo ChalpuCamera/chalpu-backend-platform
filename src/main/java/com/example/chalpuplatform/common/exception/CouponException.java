2b873bfeccefe8b6f23a99fb7b9872dbe0aa04ea
GoToBILL <wnqudwnrlrl@naver.com>
2025-10-20 16:02:37 +0900

    feat: 쿠폰 시스템을 위한 예외 처리를 추가했습니다

---
+++ src/main/java/com/example/chalpuplatform/common/exception/CouponException.java
@@ -0,0 +1,8 @@
+package com.example.chalpuplatform.common.exception;
+
+public class CouponException extends BaseException {
+
+    public CouponException(ErrorMessage errorMessage) {
+        super(errorMessage);
+    }
+}
