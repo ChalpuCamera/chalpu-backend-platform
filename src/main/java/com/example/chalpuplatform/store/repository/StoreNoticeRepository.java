ed538616fd3dabcc119720b88b7a975e3d234452
GoToBILL <wnqudwnrlrl@naver.com>
2025-11-03 17:03:39 +0900

    feat: StoreNoticeRepository에 대표 공지사항 조회 메서드를 추가했습니다
    - findByStoreIdAndIsRepresentativeTrue 메서드 추가
    - 가게별 대표 공지사항 조회 기능

--- src/main/java/com/example/chalpuplatform/store/repository/StoreNoticeRepository.java
+++ src/main/java/com/example/chalpuplatform/store/repository/StoreNoticeRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StoreNoticeRepository extends JpaRepository<StoreNotice, Long> {
 
     Page<StoreNotice> findByStoreId(Long storeId, Pageable pageable);
+
+    Optional<StoreNotice> findByStoreIdAndIsRepresentativeTrue(Long storeId);
 }
