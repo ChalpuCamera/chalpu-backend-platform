e92d70bd8afcf6f7d40183af9f8fa62aedb2f0d1
GoToBILL <wnqudwnrlrl@naver.com>
2025-11-03 17:04:47 +0900

    refactor: BaseTimeEntity에 updatedAt 필드를 추가하고 Getter를 추가했습니다
    - updatedAt 필드 추가로 수정 시간 추적 기능 추가
    - @Getter 어노테이션 추가
    - createdAt에 updatable = false 속성 추가
    - 코드 포맷팅 개선

--- src/main/java/com/example/chalpuplatform/common/test/BaseTimeEntity.java
+++ src/main/java/com/example/chalpuplatform/common/test/BaseTimeEntity.java
@@ -3,17 +3,22 @@ package com.example.chalpuplatform.common.test;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
     @CreatedDate
-    @Column(name ="createdAt", nullable = false)
+    @Column(name = "createdAt", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-
+    @LastModifiedDate
+    @Column(name = "updatedAt", nullable = false)
+    private LocalDateTime updatedAt;
 }
